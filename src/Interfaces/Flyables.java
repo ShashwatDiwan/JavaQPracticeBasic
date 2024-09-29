c873e476716e459627dbaaab4bc9e1cc275f0a4e
Shashwat Diwan <shashh.code@gmail.com>
2024-09-29 21:22:00 +0530

    commit.1.3 29th Sept

--- src/Interfaces/Flyables.java
+++ src/Interfaces/Flyables.java
@@ -1,6 +1,6 @@
 //Write a Java program to create an interface Flyable with a method called fly_obj().
 // Create three classes Spacecraft, Airplane, and Helicopter that implement the Flyable interface.
-// Implement the fly_obj() method for each of the three classes.
+// Implement the fly_obj() method for each of the three classes...
 package Interfaces;
 
 interface Flyable{
