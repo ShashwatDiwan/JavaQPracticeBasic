36fbb53743d6691a844cdea5d46c87eb6c297475
Shashwat Diwan <shashh.code@gmail.com>
2024-10-23 23:43:25 +0530

    Commit 4.0 23rd October

---
+++ src/Interfaces/bankingSystem.java
@@ -0,0 +1,17 @@
+//4. Write a Java program to create a banking system with three classes - Bank Account, SavingsAccount, and CurrentAccount.
+//The bank should have a list of accounts and methods for adding them.
+// Accounts should be an interface with methods to deposit, withdraw, calculate interest, and view balances.
+//. SavingsAccount and CurrentAccount should implement the Account interface and have their own unique methods.
+
+package Interfaces;
+
+interface Accounts{
+
+
+}
+
+public class bankingSystem {
+    public static void main(String[] args) {
+
+    }
+}
